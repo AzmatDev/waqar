cccb631c7aa302a2aadd0ae6cef2e276b8e3c0cf
AzmatDev <azmat.chwt@gmail.com>
2026-05-15 20:38:25 +0200

    Affiche erreur mail-2

--- api/send-mail.js
+++ api/send-mail.js
@@ -1,6 +1,15 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async function handler(req, res) {
+    // Gère le preflight CORS
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     if (req.method !== 'POST') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
